5eecf4d49dc4e369bff63b530b9fcc439e86bdb0
David Tei <daveteidt@gmail.com>
2023-06-24 14:26:30 +0000

    Modify console

--- console.py
+++ console.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 """ Console Module """
 import cmd
-import sys
 from shlex import split
 from datetime import datetime
 from models.base_model import BaseModel
 from models import storage
-from models.__init__ import storage
 from models.user import User
 from models.place import Place
 from models.state import State
@@ -18,8 +16,7 @@ from models.review import Review
 class HBNBCommand(cmd.Cmd):
     """Contains the functionality for the HBNB console"""
 
-    # determines prompt for interactive/non-interactive modes
-    prompt = '(hbnb) ' if sys.__stdin__.isatty() else ''
+    prompt = "(hbnb) "
 
     classes = {
         "BaseModel",
@@ -30,74 +27,6 @@ class HBNBCommand(cmd.Cmd):
         "Place",
         "Review"
     }
-    dot_cmds = ['all', 'count', 'show', 'destroy', 'update']
-    types = {
-             'number_rooms': int, 'number_bathrooms': int,
-             'max_guest': int, 'price_by_night': int,
-             'latitude': float, 'longitude': float
-            }
-
-    def preloop(self):
-        """Prints if isatty is false"""
-        if not sys.__stdin__.isatty():
-            print('(hbnb)')
-
-    def precmd(self, line):
-        """Reformat command line for advanced command syntax.
-
-        Usage: <class name>.<command>([<id> [<*args> or <**kwargs>]])
-        (Brackets denote optional fields in usage example.)
-        """
-        _cmd = _cls = _id = _args = ''  # initialize line elements
-
-        # scan for general formating - i.e '.', '(', ')'
-        if not ('.' in line and '(' in line and ')' in line):
-            return line
-
-        try:  # parse line left to right
-            pline = line[:]  # parsed line
-
-            # isolate <class name>
-            _cls = pline[:pline.find('.')]
-
-            # isolate and validate <command>
-            _cmd = pline[pline.find('.') + 1:pline.find('(')]
-            if _cmd not in HBNBCommand.dot_cmds:
-                raise Exception
-
-            # if parantheses contain arguments, parse them
-            pline = pline[pline.find('(') + 1:pline.find(')')]
-            if pline:
-                # partition args: (<id>, [<delim>], [<*args>])
-                pline = pline.partition(', ')  # pline convert to tuple
-
-                # isolate _id, stripping quotes
-                _id = pline[0].replace('\"', '')
-                # possible bug here:
-                # empty quotes register as empty _id when replaced
-
-                # if arguments exist beyond _id
-                pline = pline[2].strip()  # pline is now str
-                if pline:
-                    # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is '}'\
-                            and type(eval(pline)) is dict:
-                        _args = pline
-                    else:
-                        _args = pline.replace(',', '')
-                        # _args = _args.replace('\"', '')
-            line = ' '.join([_cmd, _cls, _id, _args])
-
-        except Exception as mess:
-            pass
-        finally:
-            return line
-
-    def postcmd(self, stop, line):
-        """Prints if isatty is false"""
-        if not sys.__stdin__.isatty():
-            print('(hbnb) ', end='')
-        return stop
 
     def do_quit(self, command):
         """ Method to exit the HBNB console"""
@@ -193,7 +122,7 @@ class HBNBCommand(cmd.Cmd):
         print("Shows an individual instance of a class")
         print("[Usage]: show <className> <objectId>\n")
 
-    def do_destroy(self, arg):
+    def do_destroy(self, line):
         """ Destroys a specified object
         Exceptions:
             SyntaxError: when there is no args given
@@ -202,9 +131,9 @@ class HBNBCommand(cmd.Cmd):
             KeyError: when there is no valid id given
         """
         try:
-            if not arg:
+            if not line:
                 raise SyntaxError()
-            my_list = arg.split(" ")
+            my_list = line.split(" ")
             if my_list[0] not in self.__classes:
                 raise NameError()
             if len(my_list) < 2:
@@ -230,22 +159,20 @@ class HBNBCommand(cmd.Cmd):
         print("Destroys an individual instance of a class")
         print("[Usage]: destroy <className> <objectId>\n")
 
-    def do_all(self, arg):
+    def do_all(self, line):
         """ Shows all objects, or all objects of a class"""
-        print_list = []
-
-        if not arg:
+        if not line:
             o = storage.all()
             print([o[k].__str__() for k in o])
             return
+
         try:
-            args = arg.split(" ")
+            args = line.split(" ")
             if args[0] not in self.__classes:
                 raise NameError()
 
             o = storage.all(eval(args[0]))
             print([o[k].__str__() for k in o])
-
         except NameError:
             print("** class doesn't exist **")
 
@@ -254,21 +181,14 @@ class HBNBCommand(cmd.Cmd):
         print("Shows all objects, or all of a class")
         print("[Usage]: all <className>\n")
 
-    def do_count(self, args):
-        """Count current number of class instances"""
-        count = 0
-        for k, v in storage._FileStorage__objects.items():
-            if args == k.split('.')[0]:
-                count += 1
-        print(count)
-
     def help_count(self):
-        """ """
+        """Help information for count"""
         print("Usage: count <class_name>")
 
     def count(self, line):
         """ Count the number of instances of a class """
         counter = 0
+
         try:
             my_list = split(line, " ")
             if my_list[0] not in self.__classes:
@@ -361,6 +281,7 @@ class HBNBCommand(cmd.Cmd):
             returns string of arguments
         """
         new_list = []
+
         new_list.append(args[0])
         try:
             my_dict = eval(
