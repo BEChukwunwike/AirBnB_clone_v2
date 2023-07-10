1fb8cf9310b42cd3636ea5095453e04abf3fe2ce
David Tei <daveteidt@gmail.com>
2023-07-10 03:17:31 +0000

    Added setup_web_static script

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# A Bash script that sets up your web servers for the deployment of web_static
+apt-get -y update
+apt-get -y install nginx
+mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo "AirBnB Clone test" | tee /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -hR ubuntu:ubuntu /data/
+sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+service nginx start
